d9c736fcc9598dc536580fca1900fd95e5d7b9dc
ljubicanstvena16 <joksiiib@gmail.com>
2023-09-15 17:48:20 +0200

    Dodavanje autentifikacije i zastite ruta na projektu

--- app/routes/api.php
+++ app/routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ClanController;
+use App\Http\Controllers\PozajmicaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\API\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,24 +13,34 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Auth
+
+Route::post('/registracija', [AuthController::class, 'registracija']);
+Route::post('/prijava', [AuthController::class, 'prijava']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('/odjava', [AuthController::class, 'odjava']);
+
+    Route::resource('clans', ClanController::class);
+    Route::get('/clans', [ClanController::class, 'vratiSveClanove']);
+    Route::post('/clans', [ClanController::class, 'dodajClana']);
+    Route::delete('/clans/{clan}', [ClanController::class, 'obrisiClana']);
+
+
+    Route::resource('pozajmicas', PozajmicaController::class);
+    Route::get('/pozajmicas', [PozajmicaController::class, 'vratiSvePozajmice']);
+    Route::post('/pozajmicas', [PozajmicaController::class, 'dodajPozajmicu']);
+    Route::delete('/pozajmicas/{pozajmica}', [PozajmicaController::class, 'obrisiPozajmicu']);
+
+
 });
-
-Route::resource('clans', ClanController::class);
-
-Route::get('/clans', [ClanController::class, 'vratiSveClanove']);
-Route::post('/clans', [ClanController::class, 'dodajClana']);
-Route::delete('/clans/{clan}', [ClanController::class, 'obrisiClana']);
-
-
-Route::resource('pozajmicas', PozajmicaController::class);
-
-Route::get('/pozajmicas', [PozajmicaController::class, 'vratiSvePozajmice']);
-Route::post('/pozajmicas', [PozajmicaController::class, 'dodajPozajmicu']);
-Route::delete('/pozajmicas/{pozajmica}', [PozajmicaController::class, 'obrisiPozajmicu']);
